44370614b93e7dea7ad83deab5ed5902b82df889
Rafael Schenkel <rafaschenkel.dev@gmail.com>
2023-09-15 16:51:11 -0300

    update bodySchema

--- src/routes/generate-ai-completion.ts
+++ src/routes/generate-ai-completion.ts
@@ -9,11 +9,11 @@ export async function generateAiCompletion(app: FastifyInstance) {
   app.post('/ai/complete', async (req, reply) => {
     const bodySchema = z.object({
       videoId: z.string().uuid(),
-      template: z.string(),
+      prompt: z.string(),
       temperature: z.number().min(0).max(1).default(0.5)
     });
 
-    const { videoId, template, temperature } = bodySchema.parse(req.body); // recebe o ID do video, template, temperatura, passadas no corpo da requisição
+    const { videoId, prompt, temperature } = bodySchema.parse(req.body); // recebe o ID do video, prompt, temperatura, passadas no corpo da requisição
 
     const video = await prisma.video.findUniqueOrThrow({
       where: {
@@ -25,7 +25,7 @@ export async function generateAiCompletion(app: FastifyInstance) {
       return reply.status(400).send({ error: 'Video transcription was not generated yet.' });
     } // retorna uma mensagem de erro caso o video não tenha uma transcrição
 
-    const promptMessage = template.replace('{transcription}', video.transcription); // recebe o template junto com a transcrição do video
+    const promptMessage = prompt.replace('{transcription}', video.transcription); // recebe o prompt junto com a transcrição do video
 
     const response = await openai.chat.completions.create({
       model: 'gpt-3.5-turbo-16k',
